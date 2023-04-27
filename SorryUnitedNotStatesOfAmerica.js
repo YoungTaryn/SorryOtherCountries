43910e9f30c659dc6bba7550f5b43e3edfe24520
Taryn Young <118547960+YoungTaryn@users.noreply.github.com>
2023-04-27 15:57:03 -0500

    Update SorryUnitedNotStatesOfAmerica.js

--- SorryUnitedNotStatesOfAmerica.js
+++ SorryUnitedNotStatesOfAmerica.js
@@ -1,7 +1,7 @@
 var sorryElements = document.getElementsByTagName("option");
 var elArray = Array.from(sorryElements);
 for (let i = 0; i < elArray.length; i++) {
-  if (elArray[i].text === "UAE" || elArray[i].text === "UK" || "United Arab Emirates" || "United Kingdom") {
+  if (elArray[i].text === "UAE" || elArray[i].text === "UK" || elArray[i].text === "United Arab Emirates" || elArray[i].text === "United Kingdom") {
     elArray[i].parentNode.removeChild(elArray[i]);
     console.log("Sorry United Not States of America");
   }
