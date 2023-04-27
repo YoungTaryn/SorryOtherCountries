9a826cf2025918c5ec52f4df1cca2499ddb641ab
Taryn Young <118547960+YoungTaryn@users.noreply.github.com>
2023-04-27 15:55:57 -0500

    Update and rename SorryTennessee.js to SorryUnitedNotStatesOfAmerica.js
    Change from removing Tennessee to removing other countries that start with United.

--- SorryUnitedNotStatesOfAmerica.js
+++ SorryUnitedNotStatesOfAmerica.js
@@ -1,8 +1,8 @@
 var sorryElements = document.getElementsByTagName("option");
 var elArray = Array.from(sorryElements);
 for (let i = 0; i < elArray.length; i++) {
-  if (elArray[i].text === "TN" || elArray[i].text === "Tennessee") {
+  if (elArray[i].text === "UAE" || elArray[i].text === "UK" || "United Arab Emirates" || "United Kingdom") {
     elArray[i].parentNode.removeChild(elArray[i]);
-    console.log("Sorry Tennessee");
+    console.log("Sorry United Not States of America");
   }
 }
